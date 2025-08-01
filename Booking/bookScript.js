2cc7131fe55ff17cc2b9f2e26d19002b09554e04
BuwieSit <santosbuhawie28@gmail.com>
2025-08-01 12:34:23 +0700

    booking

--- Booking/bookScript.js
+++ Booking/bookScript.js
@@ -1,6 +1,29 @@
 const selectedPackage = localStorage.getItem('package');
 const bookTitle = document.querySelector('.booked-title');
-bookTitle.textContent = selectedPackage;
+const brochure = document.querySelectorAll('.brochure');
+//bookTitle.textContent = selectedPackage;
+
+ const path = window.location.pathname;
+    const depth = path.split('/').filter(Boolean).length;
+
+    let prefix = './';
+    if (depth === 2) prefix = '../';
+    if (depth >= 3) prefix = '../../';
+
+if (selectedPackage && selectedPackage.trim().toLowerCase() === 'kapwa immersion journey') {
+    // Set multiple brochure images
+    const kapwaImages = [
+        `${prefix}images/DOLLARS/kapwa_1.png`,
+        `${prefix}images/DOLLARS/kapwa_2.png` // Add more if needed
+    ];
+
+    // Set each brochure image src
+    brochure.forEach((img, index) => {
+        if (kapwaImages[index]) {
+            img.src = kapwaImages[index];
+        }
+    });
+}
 
 
 const radio = document.querySelectorAll('.form-radio');
